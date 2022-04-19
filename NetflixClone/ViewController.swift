dc05deea2a9fc57e8e8a11707fa8ad69558f7b43
Akin Aksoy <48281659+akinaksoy@users.noreply.github.com>
2022-04-19 21:29:01 +0300

    Initial commit

---
+++ NetflixClone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NetflixClone
+//
+//  Created by Akın Aksoy on 19.04.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
