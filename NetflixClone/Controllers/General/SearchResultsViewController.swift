514a2d7aaa91d19be1a0fa07325598f3312890db
Akin Aksoy <48281659+akinaksoy@users.noreply.github.com>
2022-04-22 22:09:06 +0300

    Project folder structure is up to date

---
+++ NetflixClone/Controllers/General/SearchResultsViewController.swift
@@ -0,0 +1,17 @@
+//
+//  SearchResultsViewController.swift
+//  NetflixClone
+//
+//  Created by Akın Aksoy on 22.04.2022.
+//
+
+import UIKit
+
+class SearchResultsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemGreen
+    }
+
+}
