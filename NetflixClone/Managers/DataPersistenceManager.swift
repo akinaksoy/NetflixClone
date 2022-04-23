015d6538fa4f9669ff616777499a8dd0f21862a5
Akin Aksoy <48281659+akinaksoy@users.noreply.github.com>
2022-04-23 21:28:00 +0300

    Fetch request from database function is added

--- NetflixClone/Managers/DataPersistenceManager.swift
+++ NetflixClone/Managers/DataPersistenceManager.swift
@@ -13,6 +13,7 @@ class DataPersistenceManager {
     
     enum DatabaseError : Error {
         case failedToSaveData
+        case failedToFetchData
     }
     
     
@@ -41,6 +42,25 @@ class DataPersistenceManager {
         }catch{
             completion(.failure(error))
             print(DatabaseError.failedToSaveData)
+        }
+    }
+    
+    
+    func fetchTitleFromDatabase(completion : @escaping(Result<[TitleItem],Error>)-> Void){
+        guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else {
+            return
+        }
+        let context = appDelegate.persistentContainer.viewContext
+        
+        let request : NSFetchRequest<TitleItem>
+        
+        request = TitleItem.fetchRequest()
+        
+        do {
+            let titles = try context.fetch(request)
+            completion(.success(titles))
+        }catch{
+            completion(.failure(DatabaseError.failedToFetchData))
         }
     }
     
