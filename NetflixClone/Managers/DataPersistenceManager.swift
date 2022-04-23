1d0522acb90e3f8a927118b2c20e22b9a1a72d4a
Akin Aksoy <48281659+akinaksoy@users.noreply.github.com>
2022-04-23 21:37:59 +0300

    Delete from database method added

--- NetflixClone/Managers/DataPersistenceManager.swift
+++ NetflixClone/Managers/DataPersistenceManager.swift
@@ -14,6 +14,7 @@ class DataPersistenceManager {
     enum DatabaseError : Error {
         case failedToSaveData
         case failedToFetchData
+        case failedToDeleteData
     }
     
     
@@ -61,6 +62,20 @@ class DataPersistenceManager {
             completion(.success(titles))
         }catch{
             completion(.failure(DatabaseError.failedToFetchData))
+        }
+    }
+    
+    func deleteTitle(model: TitleItem,completion: @escaping(Result<Void,Error>)->Void){
+        guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else {
+            return
+        }
+        let context = appDelegate.persistentContainer.viewContext
+        context.delete(model) // silmek için dbye istek atıyor
+        do{
+            try context.save()//silmeyi confirmliyor
+            completion(.success(()))
+        }catch{
+            completion(.failure(DatabaseError.failedToDeleteData))
         }
     }
     
